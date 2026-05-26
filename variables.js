3ae815157dc050046eea1c3a31170089d85996b4
Vutivi-chauke <vutivijajamba@gmail.com>
2026-05-26 14:28:52 +0200

    arithmetics

--- variables.js
+++ variables.js
@@ -1,3 +1,4 @@
+//variables
 name = "vutivi"
 age = 32
 isStudent = true
@@ -5,3 +6,21 @@ isStudent = true
 console.log(name)
 console.log(age)
 console.log(isStudent)
+
+//reassignment
+favouriteColor = "blue"
+favouriteColor = "red"
+console.log(favouriteColor)
+
+//Arithmetic
+num1 = 4
+num2 = 2
+
+sum = num1 + num2
+difference = num1 - num2
+product = num1 * num2
+quotient = num1 / num2
+console.log(sum)
+console.log(difference)
+console.log(product)
+console.log(quotient)
