b7565d975c177924f305ea4d3d17063bc0530227
Vutivi-chauke <vutivijajamba@gmail.com>
2026-05-26 15:16:26 +0200

    operations and boleans

---
+++ data_types.js
@@ -0,0 +1,73 @@
+//numbers
+
+number = 3
+height = 1.56
+
+addition = number + height
+subtraction = number - height
+multiplication = number * height
+division = number / height
+modulus = number % height
+exponentiation = number ** height
+exponentiation2 = number ** 2
+
+
+console.log(addition)
+console.log(subtraction)
+console.log(multiplication)
+console.log(division)
+console.log(modulus)
+console.log(exponentiation)
+console.log(exponentiation2)
+
+//Boolean and Operators
+
+
+equals_to =  5 == 10
+not_equals_to = 5 != 10
+greater_than = 5 > 10
+less_than = 5 < 10
+greater_than_equals = 5 >= 10
+less_than_equals = 5 <= 10
+
+
+x = 8
+y = 12
+
+greater_xy = x > y
+less_equal_xy = x <= y
+equals_xy = x == y
+not_equals_xy = x != y
+
+console.log(greater_xy)
+console.log(less_equal_xy)
+console.log(equals_xy)
+console.log(not_equals_xy)
+
+a = true
+b = false
+
+a_AND_b = a && b
+a_OR_b = a || b
+NOT_a = ! (a)
+console.log(a_AND_b)
+console.log(a_OR_b)
+console.log(NOT_a)
+
+p = 10
+
+p += p
+console.log(p)
+
+p -= 10
+console.log(p)
+
+p *= 5
+console.log(p)
+
+p /= 5
+console.log(p)
+
+p %= 3
+console.log(p)
+
